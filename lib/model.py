a04c1343c12eba4c1e391e5bafbeaf9c000cd9b3
Li Ling <liling@kth.se>
2020-10-16 20:30:36 +0200

    Add notes on dimensions for D2net.forward method

--- lib/model.py
+++ lib/model.py
@@ -11,22 +11,17 @@ class DenseFeatureExtractionModule(nn.Module):
 
         model = models.vgg16()
         vgg16_layers = [
-            'conv1_1', 'relu1_1', 'conv1_2', 'relu1_2',
-            'pool1',
-            'conv2_1', 'relu2_1', 'conv2_2', 'relu2_2',
-            'pool2',
-            'conv3_1', 'relu3_1', 'conv3_2', 'relu3_2', 'conv3_3', 'relu3_3',
-            'pool3',
-            'conv4_1', 'relu4_1', 'conv4_2', 'relu4_2', 'conv4_3', 'relu4_3',
-            'pool4',
+            'conv1_1', 'relu1_1', 'conv1_2', 'relu1_2', 'pool1', 'conv2_1',
+            'relu2_1', 'conv2_2', 'relu2_2', 'pool2', 'conv3_1', 'relu3_1',
+            'conv3_2', 'relu3_2', 'conv3_3', 'relu3_3', 'pool3', 'conv4_1',
+            'relu4_1', 'conv4_2', 'relu4_2', 'conv4_3', 'relu4_3', 'pool4',
             'conv5_1', 'relu5_1', 'conv5_2', 'relu5_2', 'conv5_3', 'relu5_3',
             'pool5'
         ]
         conv4_3_idx = vgg16_layers.index('conv4_3')
 
         self.model = nn.Sequential(
-            *list(model.features.children())[: conv4_3_idx + 1]
-        )
+            *list(model.features.children())[:conv4_3_idx + 1])
 
         self.num_channels = 512
 
@@ -35,7 +30,7 @@ class DenseFeatureExtractionModule(nn.Module):
             param.requires_grad = False
         if finetune_feature_extraction:
             # Unlock conv4_3
-            for param in list(self.model.parameters())[-2 :]:
+            for param in list(self.model.parameters())[-2:]:
                 param.requires_grad = True
 
         if use_cuda:
@@ -62,12 +57,10 @@ class SoftDetectionModule(nn.Module):
         max_per_sample = torch.max(batch.view(b, -1), dim=1)[0]
         exp = torch.exp(batch / max_per_sample.view(b, 1, 1, 1))
         sum_exp = (
-            self.soft_local_max_size ** 2 *
-            F.avg_pool2d(
-                F.pad(exp, [self.pad] * 4, mode='constant', value=1.),
-                self.soft_local_max_size, stride=1
-            )
-        )
+            self.soft_local_max_size**2 *
+            F.avg_pool2d(F.pad(exp, [self.pad] * 4, mode='constant', value=1.),
+                         self.soft_local_max_size,
+                         stride=1))
         local_max_score = exp / sum_exp
 
         depth_wise_max = torch.max(batch, dim=1)[0]
@@ -86,9 +79,7 @@ class D2Net(nn.Module):
         super(D2Net, self).__init__()
 
         self.dense_feature_extraction = DenseFeatureExtractionModule(
-            finetune_feature_extraction=True,
-            use_cuda=use_cuda
-        )
+            finetune_feature_extraction=True, use_cuda=use_cuda)
 
         self.detection = SoftDetectionModule()
 
@@ -96,22 +87,29 @@ class D2Net(nn.Module):
             if use_cuda:
                 self.load_state_dict(torch.load(model_file)['model'])
             else:
-                self.load_state_dict(torch.load(model_file, map_location='cpu')['model'])
+                self.load_state_dict(
+                    torch.load(model_file, map_location='cpu')['model'])
 
     def forward(self, batch):
         b = batch['image1'].size(0)
 
+        # input shape: (num_images, c_0, h_0, w_0)
+        # output(dense_features) shape: (num_images, c_f, h_f, w_f)
         dense_features = self.dense_feature_extraction(
-            torch.cat([batch['image1'], batch['image2']], dim=0)
-        )
+            torch.cat([batch['image1'], batch['image2']], dim=0))
 
+        # scores shape: (num_images, h_f, w_f)
         scores = self.detection(dense_features)
 
-        dense_features1 = dense_features[: b, :, :, :]
-        dense_features2 = dense_features[b :, :, :, :]
+        # dense_feature1 shape: (1, c_f, h_f, w_f)
+        dense_features1 = dense_features[:b, :, :, :]
+        # dense_feature2 shape: (1, c_f, h_f, w_f)
+        dense_features2 = dense_features[b:, :, :, :]
 
-        scores1 = scores[: b, :, :]
-        scores2 = scores[b :, :, :]
+        # score1 shape: (1, h_f, w_f)
+        scores1 = scores[:b, :, :]
+        # score1 shape: (1, h_f, w_f)
+        scores2 = scores[b:, :, :]
 
         return {
             'dense_features1': dense_features1,
