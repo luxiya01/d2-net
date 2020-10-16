2c1e2a16593eef0a6ecc9c856ec9fcc0ba4490a1
Li Ling <liling@kth.se>
2020-10-16 17:45:16 +0200

    SSSDataset: formalise init and load_image

--- sss_dataset.py
+++ sss_dataset.py
@@ -1,18 +1,66 @@
 """Custom PyTorch Dataset class for loading side scan sonar data"""
 
+import os
+import numpy as np
 from torch.utils.data import Dataset
 import sss_data_processing.src as dataproc
 
 
 class SSSDataset(Dataset):
     """Side scan sonar image patches dataset"""
-    def __init__(self, datadir):
-        self.datadir = datadir
+    def __init__(self,
+                 data_dir,
+                 data_indices_file,
+                 img_type='normalised',
+                 min_overlap=.3,
+                 remove_trivial_pairs=False):
+        """
+        Args:
+            - data_dir: path to the sss patch data
+            - data_indices_file: path to the text files describing the patch indices
+              to include in the dataset (e.g. for separating training and
+              testing)
+            - min_overlap: minimum amount of overlap required to be considered
+              as an overlapping image pair
+            - remove_trivial_pairs: if True, trivial overlapping pair of images
+              (patches cropped out from overlapping regions of the same sss
+              image) are removed from the dataset
+        """
+        self.data_dir = data_dir
+        self.patches_dir = os.path.join(self.data_dir, 'patches')
+        self.img_type = img_type
         self.correspondence_getter = dataproc.correspondence_getter.CorrespondenceGetter(
-            self.datadir)
+            self.data_dir, data_indices_file)
+        self.overlapping_pairs = self.correspondence_getter.get_all_pairs_with_target_overlap(
+            min_overlap=min_overlap,
+            max_overlap=.99,
+            remove_trivial_pairs=remove_trivial_pairs)
+
+    def _load_image(self, idx):
+        """Given a patch index, load the correponding img_type (normalised or
+        unnormalised), scale up intensities to (0, 255) and convert from
+        grayscale to rgb."""
+        patch_name = '.'.join([str(idx), 'npz'])
+        image = np.load(os.path.join(self.patches_dir,
+                                     patch_name))[self.img_type]
+
+        # image intensities (0, 1)-> (0, 255)
+        image *= 255
+
+        # grayscale -> 3 color channels
+        if len(image.shape) == 2:
+            image = image[:, :, np.newaxis]
+            image = np.repeat(image, 3, -1)
+        return image
 
     def __len__(self):
-        pass
+        """Number of overlapping pairs in the dataset"""
+        return len(self.overlapping_pairs)
 
-    def __getitem__(self, idx):
-        return None
+    def __getitem__(self, pair_idx):
+        idx1, idx2 = self.overlapping_pairs[pair_idx]
+        return {
+            'image1': self._load_image(idx1),
+            'image2': self._load_image(idx2),
+            'overlap': self.correspondence_getter.overlap_matrix[idx1, idx2]
+        }
