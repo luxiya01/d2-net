37a3a424eba9c952abc6f2d729a052de1ae00ce8
Li Ling <liling@kth.se>
2020-10-22 16:43:05 +0200

    Move argparser to a new file

---
+++ parse_args.py
@@ -0,0 +1,117 @@
+import argparse
+
+
+# Argument parsing
+def get_args():
+    parser = argparse.ArgumentParser(description='Training script')
+
+    parser.add_argument('--data_dir',
+                        type=str,
+                        required=True,
+                        help='path to the dataset')
+    parser.add_argument(
+        '--data_indices_file',
+        type=str,
+        required=True,
+        help=
+        'path to the data indices file containing patch indices used for training'
+    )
+    parser.add_argument(
+        '--remove_trivial_pairs',
+        action='store_true',
+        help=
+        'whether trivial overlapping pairs should be removed from the dataset')
+    parser.add_argument(
+        '--img_type',
+        type=str,
+        default='norm_intensity',
+        help='image type used (norm_intensity or unnorm_intensity)')
+    parser.add_argument(
+        '--min_overlap',
+        type=float,
+        default=.3,
+        help='minimum overlap required to be considered as an overlapping pair'
+    )
+    parser.add_argument(
+        '--max_overlap',
+        type=float,
+        default=.99,
+        help='maximum overlap required to be considered as an overlapping pair'
+    )
+    parser.add_argument(
+        '--pos_round_to',
+        type=int,
+        default=5,
+        help=
+        'the decimals that the physical positions will be rounded to during correspondence calculation, pos_round_to = 5 -> round to closest 0.2'
+    )
+    parser.add_argument(
+        '--plot_gt_correspondence',
+        action='store_true',
+        help='plot groundtruth correspondences used for training')
+
+    parser.add_argument('--preprocessing',
+                        type=str,
+                        default='torch',
+                        help='image preprocessing (caffe or torch)')
+    parser.add_argument('--model_file',
+                        type=str,
+                        default='models/d2_tf.pth',
+                        help='path to the full model')
+
+    parser.add_argument('--num_epochs',
+                        type=int,
+                        default=10,
+                        help='number of training epochs')
+    parser.add_argument('--lr',
+                        type=float,
+                        default=1e-3,
+                        help='initial learning rate')
+    parser.add_argument('--batch_size', type=int, default=1, help='batch size')
+    parser.add_argument('--num_workers',
+                        type=int,
+                        default=4,
+                        help='number of workers for data loading')
+
+    parser.add_argument('--use_validation',
+                        dest='use_validation',
+                        action='store_true',
+                        help='use the validation split')
+    parser.set_defaults(use_validation=False)
+    parser.add_argument(
+        '--validation_size',
+        type=float,
+        default=.1,
+        help='percentage of the dataset used as validation set')
+
+    parser.add_argument('--log_interval',
+                        type=int,
+                        default=50,
+                        help='loss logging interval')
+
+    parser.add_argument('--log_file',
+                        type=str,
+                        default='log.txt',
+                        help='loss logging file')
+    parser.add_argument('--log_dir',
+                        type=str,
+                        required=True,
+                        help='logging directory')
+
+    parser.add_argument('--plot',
+                        dest='plot',
+                        action='store_true',
+                        help='plot training pairs')
+    parser.set_defaults(plot=True)
+
+    parser.add_argument('--checkpoint_directory',
+                        type=str,
+                        default='checkpoints',
+                        help='directory for training checkpoints')
+    parser.add_argument('--checkpoint_prefix',
+                        type=str,
+                        default='d2',
+                        help='prefix for training checkpoints')
+
+    args = parser.parse_args()
+    return args
--- train.py
+++ train.py
@@ -1,5 +1,3 @@
-import argparse
-
 import numpy as np
 
 import os
@@ -19,6 +17,7 @@ from lib.sss_dataset import SSSDataset
 from lib.exceptions import NoGradientError
 from lib.loss import loss_function
 from lib.model import D2Net
+import parse_args
 
 # CUDA
 use_cuda = torch.cuda.is_available()
@@ -30,88 +29,7 @@ if use_cuda:
     torch.cuda.manual_seed(1)
 np.random.seed(1)
 
-# Argument parsing
-parser = argparse.ArgumentParser(description='Training script')
-
-parser.add_argument('--data_dir',
-                    type=str,
-                    required=True,
-                    help='path to the dataset')
-parser.add_argument(
-    '--remove_trivial_pairs',
-    action='store_true',
-    help='whether trivial overlapping pairs should be removed from the dataset'
-)
-parser.add_argument(
-    '--img_type',
-    type=str,
-    default='norm_intensity',
-    help='image type used (norm_intensity or unnorm_intensity)')
-parser.add_argument(
-    '--min_overlap',
-    type=float,
-    default=.3,
-    help='minimum overlap required to be considered as an overlapping pair')
-
-parser.add_argument('--preprocessing',
-                    type=str,
-                    default='torch',
-                    help='image preprocessing (caffe or torch)')
-parser.add_argument('--model_file',
-                    type=str,
-                    default='models/d2_tf.pth',
-                    help='path to the full model')
-
-parser.add_argument('--num_epochs',
-                    type=int,
-                    default=10,
-                    help='number of training epochs')
-parser.add_argument('--lr',
-                    type=float,
-                    default=1e-3,
-                    help='initial learning rate')
-parser.add_argument('--batch_size', type=int, default=1, help='batch size')
-parser.add_argument('--num_workers',
-                    type=int,
-                    default=4,
-                    help='number of workers for data loading')
-
-parser.add_argument('--use_validation',
-                    dest='use_validation',
-                    action='store_true',
-                    help='use the validation split')
-parser.set_defaults(use_validation=True)
-parser.add_argument('--validation_size',
-                    type=float,
-                    default=.1,
-                    help='percentage of the dataset used as validation set')
-
-parser.add_argument('--log_interval',
-                    type=int,
-                    default=50,
-                    help='loss logging interval')
-
-parser.add_argument('--log_file',
-                    type=str,
-                    default='log.txt',
-                    help='loss logging file')
-
-parser.add_argument('--plot',
-                    dest='plot',
-                    action='store_true',
-                    help='plot training pairs')
-parser.set_defaults(plot=True)
-
-parser.add_argument('--checkpoint_directory',
-                    type=str,
-                    default='checkpoints',
-                    help='directory for training checkpoints')
-parser.add_argument('--checkpoint_prefix',
-                    type=str,
-                    default='d2',
-                    help='prefix for training checkpoints')
-
-args = parser.parse_args()
+args = parse_args.get_args()
 
 print(args)
 
@@ -131,13 +49,17 @@ optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()),
                        lr=args.lr)
 
 # Dataset
-training_dataset = SSSDataset(data_dir=args.data_dir,
-                              data_indices_file=os.path.join(
-                                  args.data_dir, 'train.idx'),
-                              remove_trivial_pairs=args.remove_trivial_pairs,
-                              preprocessing=args.preprocessing,
-                              img_type=args.img_type,
-                              min_overlap=args.min_overlap)
+training_dataset = SSSDataset(
+    data_dir=args.data_dir,
+    data_indices_file=args.data_indices_file,
+    remove_trivial_pairs=args.remove_trivial_pairs,
+    preprocessing=args.preprocessing,
+    img_type=args.img_type,
+    min_overlap=args.min_overlap,
+    max_overlap=args.max_overlap,
+    pos_round_to=args.pos_round_to,
+    plot_gt_correspondence=args.plot_gt_correspondence)
+print(f'Num training data: {len(training_dataset)}')
 if args.use_validation:
     num_validation = max(int(len(training_dataset) * args.validation_size), 1)
     num_train = len(training_dataset) - num_validation
