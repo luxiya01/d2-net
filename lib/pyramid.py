fdb936b67c3b9cd5bcc5c803c9b93c3b856229b9
Li Ling <liling@kth.se>
2020-11-01 01:10:18 +0100

    Pylint formating

--- lib/pyramid.py
+++ lib/pyramid.py
@@ -9,21 +9,20 @@ from lib.utils import interpolate_dense_features, upscale_positions
 def process_multiscale(image, model, scales=[.5, 1, 2]):
     b, _, h_init, w_init = image.size()
     device = image.device
-    assert(b == 1)
+    assert (b == 1)
 
     all_keypoints = torch.zeros([3, 0])
-    all_descriptors = torch.zeros([
-        model.dense_feature_extraction.num_channels, 0
-    ])
+    all_descriptors = torch.zeros(
+        [model.dense_feature_extraction.num_channels, 0])
     all_scores = torch.zeros(0)
 
     previous_dense_features = None
     banned = None
     for idx, scale in enumerate(scales):
-        current_image = F.interpolate(
-            image, scale_factor=scale,
-            mode='bilinear', align_corners=True
-        )
+        current_image = F.interpolate(image,
+                                      scale_factor=scale,
+                                      mode='bilinear',
+                                      align_corners=True)
         _, _, h_level, w_level = current_image.size()
 
         dense_features = model.dense_feature_extraction(current_image)
@@ -33,10 +32,10 @@ def process_multiscale(image, model, scales=[.5, 1, 2]):
 
         # Sum the feature maps.
         if previous_dense_features is not None:
-            dense_features += F.interpolate(
-                previous_dense_features, size=[h, w],
-                mode='bilinear', align_corners=True
-            )
+            dense_features += F.interpolate(previous_dense_features,
+                                            size=[h, w],
+                                            mode='bilinear',
+                                            align_corners=True)
             del previous_dense_features
 
         # Recover detections.
@@ -45,8 +44,7 @@ def process_multiscale(image, model, scales=[.5, 1, 2]):
             banned = F.interpolate(banned.float(), size=[h, w]).bool()
             detections = torch.min(detections, ~banned)
             banned = torch.max(
-                torch.max(detections, dim=1)[0].unsqueeze(1), banned
-            )
+                torch.max(detections, dim=1)[0].unsqueeze(1), banned)
         else:
             banned = torch.max(detections, dim=1)[0].unsqueeze(1)
         fmap_pos = torch.nonzero(detections[0].cpu()).t()
@@ -54,33 +52,26 @@ def process_multiscale(image, model, scales=[.5, 1, 2]):
 
         # Recover displacements.
         displacements = model.localization(dense_features)[0].cpu()
-        displacements_i = displacements[
-            0, fmap_pos[0, :], fmap_pos[1, :], fmap_pos[2, :]
-        ]
-        displacements_j = displacements[
-            1, fmap_pos[0, :], fmap_pos[1, :], fmap_pos[2, :]
-        ]
+        displacements_i = displacements[0, fmap_pos[0, :], fmap_pos[1, :],
+                                        fmap_pos[2, :]]
+        displacements_j = displacements[1, fmap_pos[0, :], fmap_pos[1, :],
+                                        fmap_pos[2, :]]
         del displacements
 
         mask = torch.min(
             torch.abs(displacements_i) < 0.5,
-            torch.abs(displacements_j) < 0.5
-        )
+            torch.abs(displacements_j) < 0.5)
         fmap_pos = fmap_pos[:, mask]
-        valid_displacements = torch.stack([
-            displacements_i[mask],
-            displacements_j[mask]
-        ], dim=0)
+        valid_displacements = torch.stack(
+            [displacements_i[mask], displacements_j[mask]], dim=0)
         del mask, displacements_i, displacements_j
 
-        fmap_keypoints = fmap_pos[1 :, :].float() + valid_displacements
+        fmap_keypoints = fmap_pos[1:, :].float() + valid_displacements
         del valid_displacements
 
         try:
             raw_descriptors, _, ids = interpolate_dense_features(
-                fmap_keypoints.to(device),
-                dense_features[0]
-            )
+                fmap_keypoints.to(device), dense_features[0])
         except EmptyTensorError:
             continue
         fmap_pos = fmap_pos[:, ids]
@@ -102,11 +93,11 @@ def process_multiscale(image, model, scales=[.5, 1, 2]):
         keypoints = torch.cat([
             keypoints,
             torch.ones([1, keypoints.size(1)]) * 1 / scale,
-        ], dim=0)
+        ],
+                              dim=0)
 
-        scores = dense_features[
-            0, fmap_pos[0, :], fmap_pos[1, :], fmap_pos[2, :]
-        ].cpu() / (idx + 1)
+        scores = dense_features[0, fmap_pos[0, :], fmap_pos[1, :],
+                                fmap_pos[2, :]].cpu() / (idx + 1)
         del fmap_pos
 
         all_keypoints = torch.cat([all_keypoints, keypoints], dim=1)
